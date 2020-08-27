74bc02785d47fada888aa6de472540757f49f558
Shreyhas <shreyhas@gmail.com>
2020-08-27 09:14:35 +0530

    Add to repository

---
+++ Iterate.py
@@ -0,0 +1,22 @@
+
+class Iterate():
+
+    def __init__(self):
+        pass
+
+    def generate_dict(self):
+        """
+        make a dictionary for each word in the file and associate locations with word
+        :return: dictionary
+        """
+        pass
+
+    def generate_reverse_index(self):
+        """
+        generate a reverse index using the content from the dictionary
+        :return:
+        """
+        pass
+
+
+
