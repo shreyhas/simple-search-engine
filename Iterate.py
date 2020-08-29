d2c11e9b4149b638f51bdcdcfaf231380ccfc0f5
Shreyhas <shreyhas@gmail.com>
2020-08-29 17:44:21 +0530

    Implement doc_to_df

--- Iterate.py
+++ Iterate.py
@@ -1,17 +1,41 @@
+from _collections import defaultdict
+import pandas as pd
 
 class Iterate():
     """
     Iterates over .txt files, generates a dictionary, and then generates a reverse index
     """
-    def __init__(self):
+
+    def __init__(self, no_of_docs):
+        self.no_of_docs = no_of_docs
         pass
+
 
     def generate_dict(self):
         """
-        make a dictionary for each word in the file and associate locations with word
+        make a dictionary for each word in the dataframe and associate locations with word
         :return: dictionary
         """
+        dict = defaultdict(list)
+        for i in range(self.no_of_docs-1):
+            doc_txt = self.doc_to_df(i)
+            #assign key to index in dictionary and its locations as tuples(docid,line,wordpos) as the values
         pass
+
+    def doc_to_df(self, doc_no):
+        """
+        take the document and convert it into a df with indexes of line number
+        :param doc_no: filename of document
+        :return: doc_txt
+        """
+        doc_txt = pd.DataFrame()
+        i = 1
+        with open ('{doc_id}.txt'.format(doc_id = doc_no)) as file:
+            for line in file:
+                words = pd.Series(line.split(' '))
+                doc_txt = doc_txt.append(words, ignore_index=True)
+        return doc_txt
+
 
     def generate_reverse_index(self):
         """
